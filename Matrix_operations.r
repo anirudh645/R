8710d2c4f181e6a56374993b6dc6ff3eb7502144
anirudh645 <anirudh268409@gmail.com>
2024-02-25 14:18:12 +0530

    Matrix operation multiplication added but still in progress

--- Matrix_operations.r
+++ Matrix_operations.r
@@ -1,7 +1,6 @@
-matrix1 <- matrix(c(1, 2, 3, 4, 5, 6), nrow = 3, ncol = 2)
-matrix2 <- matrix(c(1, 2, 3, 4, 5, 6), nrow = 3, ncol = 2)
-
-print(matrix1 + matrix2)
+matrix1 <- matrix(c(1, 2, 3, 4), nrow = 2, ncol = 2)
+matrix2 <- matrix(c(5, 6, 7, 8), nrow = 2, ncol = 2)
+matrix3 <- matrix(0, nrow = 2, ncol = 2)
 print("Addition of two matrices : ")
 for (i in seq_len(nrow(matrix1))){
   for (j in seq_len(ncol(matrix1))){
@@ -9,3 +8,15 @@ for (i in seq_len(nrow(matrix1))){
   }
   cat("\n")
 }
+cat(matrix1*matrix2)
+cat("\n")
+cat("Multiplication of two matrices is : \n")
+for (i in seq_len(nrow(matrix1))){
+  for (j in seq_len(ncol(matrix2))){
+    for (k in seq_len(ncol(matrix1))){
+      matrix3[i, j] <- matrix3[i, j] + matrix1[i, k] * matrix2[k, j]
+    }
+  }
+}
+
+print(t(matrix1))
