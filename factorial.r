dadcbefee0a6720372a00c894271b6fab9c755f9
anirudh645 <anirudh268409@gmail.com>
2024-03-05 14:00:48 +0530

    add 0 condition and negative number condition

--- factorial.r
+++ factorial.r
@@ -1,6 +1,12 @@
 n <- as.integer(readline("Enter a number : "))
-f <- 1
-for (i in 1:n){
-  f <- f * i
+if (n == 0) {
+  cat("Factorial of 0 is : 1\n")
+}else if (n > 0) {
+  f <- 1
+  for (i in 1:n){
+    f <- f * i
+  }
+  cat("Factorial of ", n, " is : ", f, "\n")
+}else {
+  cat("Factorial of negative number is undefined.\n")
 }
-cat("Factorial of ", n, " is : ", f, "\n")
